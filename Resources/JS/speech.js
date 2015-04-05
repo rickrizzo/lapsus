153f602ae437ec361a772697139256601aa42fde
Rob Russo <rrusso44@gmail.com>
2015-04-04 22:03:11 -0400

    Speech Word Detection
    Now detects words (again)

--- Resources/JS/speech.js
+++ Resources/JS/speech.js
@@ -2,42 +2,54 @@
 var two_line = /\n\n/g;
 var one_line = /\n/g;
 var first_char = /\S/;
-var words = ["um", "uh", "like"];
+var transcript;
+var wordMap = {};
+var fillerWords = ["uh", "um", "so", "like", "yeah"];
+var weakWords = 2;
 
 //Upgrade Broswer
 function upgrade() {
-    "use strict";
+  "use strict";
 	window.alert("Please upgrade your browser! (Try Switching to Chrome)");
 }
 
 //Linebreak
 function linebreak(s) {
-    "use strict";
+  "use strict";
 	return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
 }
 
-//Detect Filler Words
-function filler(s) {
-    "use strict";
-	
-	//var tempSpeech = speech.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-
-	//Make Array
-	var i, j;
-    s = s.split(" ");
-	
-	
-	//Find Words
-	for (i = 0; i < s.length; i += 1) {
-		for (j = 0; j < words.length; j += 1) {
-            //Check for Filler Words
-            if (s[i].toUpperCase() === words[j].toUpperCase()) {
-                s[i] = "<i>" + s[i] + "</i>";
-            }
+//Check Filler Words
+function checkFillers() {
+  //Variables
+  var i, j;
+  var speech = final_transcript;
+  
+  //Create Speech Array
+  speech = speech.split(" ");
+  
+  for (i = 0; i < speech.length; i += 1) {
+    
+    //Check Words
+    for (j = 0; j < fillerWords.length; j += 1) {
+      if (speech[i].toLowerCase() == fillerWords[j]) {
+        if (j - weakWords < 0) {
+          speech[i] = "<em>" + speech[i] + "</em>";
+        } else {
+          speech[i] = "<strong>" + speech[i] + "</strong>";
         }
-	}
-	s = s.join(" ");
-	return s;
+      } 
+    }
+    
+    //Map Words
+    if (speech[i] in wordMap) {
+      wordMap[speech] += 1;
+    } else {
+      wordMap[speech] = 0;
+    }
+  }
+  
+  document.getElementById("final_span").innerHTML = speech.join(" ");
 }
 
 //Browser Check
@@ -53,9 +65,9 @@ if (!('webkitSpeechRecognition' in window)) {
 	var speechIndex = 0;*/
 	
 
-    //Recognition Variable
-    var recognizing = false;
-    var recognition = new webkitSpeechRecognition();
+  //Recognition Variable
+  var recognizing = false;
+  var recognition = new webkitSpeechRecognition();
 	var ignore_onend;
 	
 	//API Options
@@ -65,33 +77,33 @@ if (!('webkitSpeechRecognition' in window)) {
 	
 	//On Start
 	recognition.onstart = function () {
-        "use strict";
+    "use strict";
 		recognizing = true;
 		/*speech = document.getElementById("textarea").value;*/
 	};
 	
 	//On Result
 	recognition.onresult = function (event) {
-        "use strict";
-        var i, transcript, interim_transcript = "";
-	    for (i = event.resultIndex; i < event.results.length; i += 1) {
-	        if (event.results[i].isFinal) {
-	            transcript = event.results[i][0].transcript;
-	            transcript = filler(transcript);
-	            final_transcript += transcript;
-	        } else {
-	            interim_transcript += event.results[i][0].transcript;
-	        }
+    "use strict";
+    var i, transcript, interim_transcript = "";
+	  for (i = event.resultIndex; i < event.results.length; i += 1) {
+      if (event.results[i].isFinal) {
+	      transcript = event.results[i][0].transcript;
+	      final_transcript += transcript;
+	    } else {
+	      interim_transcript += event.results[i][0].transcript;
 	    }
+	  }
 
-        //Display Text
+    //Display Text
 		document.getElementById("final_span").innerHTML = linebreak(final_transcript);
-	    document.getElementById("interim_span").innerHTML = linebreak(interim_transcript);
+    document.getElementById("interim_span").innerHTML = linebreak(interim_transcript);
+    transcript = final_transcript;
 	};
-	
+	22
 	//On Error
 	recognition.onerror = function (event) {
-        "use strict";
+    "use strict";
 		//Silence
 		if (event.error === 'no-speech') {
 			window.alert("Speak up!");
@@ -106,13 +118,14 @@ if (!('webkitSpeechRecognition' in window)) {
 		if (event.error === 'not-allowed') {
 			window.alert("You can't do that");
 			ignore_onend = true;
-        }
+    }
 	};
 	
 	//On End
 	recognition.onend = function () {
-        "use strict";
+    "use strict";
 		recognizing = false;
+    checkFillers();
         
         //Ignore End
 		if (ignore_onend) {
@@ -123,28 +136,8 @@ if (!('webkitSpeechRecognition' in window)) {
 			return;
 		}
 		if (window.getSelection) {
-            window.alert("NOT READY YET");
 			/*window.getSelection().removeAllRanges();
 			var range = document.createRange();*/
 		}
 	};
 }
-
-//Map Speech
-/*function mapSpeech(s) {
-	"use strict";
-	var i, sArr, wordMap = {};
-	sArr = s;
-	sArr = sArr.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-	sArr = sArr.split(" ");
-	
-	for (i = 0; i < sArr.length; i += 1) {
-		if (sArr[i] in wordMap) {
-			wordMap[sArr[i]] += 1;
-		} else {
-			wordMap[sArr[i]] = 1;
-		}
-	}
-	
-	window.console.log(wordMap);
-}*/
