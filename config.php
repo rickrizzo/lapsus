c486b11f772597296439f95bc74d3a0f499169af
machay <machay@rpi.edu>
2015-05-10 13:16:05 -0400

    Commented config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
-//Assigns session variables to corresponding hostname, database name, and database host
 <?php 
   $_SESSION['HOSTNAME'] = 'localhost';
   $_SESSION['DB_NAME']  = 'lapsus';
   $_SESSION['DB_HOST']  = 'localhost';
+  //Assigns session variables to corresponding hostname, database name, and database host
 ?>
