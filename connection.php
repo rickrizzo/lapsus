20dc52f63ec762c1140a4f67238d2dc9dfbbe5fb
machay <machay@rpi.edu>
2015-05-10 13:13:27 -0400

    Commented connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,12 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "lapsus";
-$conn = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
-mysql_select_db($mysql_database, $conn) or die("Could not select database");
-$mysqli = new MySQLi($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die(mysqli_error());
-$connpdo = new PDO('mysql:host=localhost;dbname='.$mysql_database, $mysql_user, $mysql_password);
+  /*The following creates a connection variable with the information needed to connect. 
+  This variable is used in the other php files*/
+  $mysql_hostname = "localhost";
+  $mysql_user = "root";
+  $mysql_password = "";
+  $mysql_database = "lapsus";
+  $conn = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+  mysql_select_db($mysql_database, $conn) or die("Could not select database"); 
+  $mysqli = new MySQLi($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die(mysqli_error());
+  $connpdo = new PDO('mysql:host=localhost;dbname='.$mysql_database, $mysql_user, $mysql_password);
 ?>
