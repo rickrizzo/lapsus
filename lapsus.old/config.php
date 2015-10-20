e387264108426cc6c464fc7891b7993033e2642e
Robert Russo <rrusso44@gmail.com>
2015-10-20 19:38:59 -0400

    Removed Lapsus.old
    The old lapsus can now be accessed in the lapsus-2014 branch

--- lapsus.old/config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  $_SESSION['HOSTNAME'] = 'localhost';
-  $_SESSION['DB_NAME']  = 'lapsus';
-  $_SESSION['DB_HOST']  = 'localhost';
-  //Assigns session variables to corresponding hostname, database name, and database host
-?>
