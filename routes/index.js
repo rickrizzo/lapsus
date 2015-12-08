c687bcdaaaf77d2efb0164836a6e80d28d4038db
Robert Russo <rrusso44@gmail.com>
2015-12-08 16:47:56 -0500

    Login
    some light routing and the early start of a login page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lapsus' });
 });
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login'});
+});
 
 module.exports = router;
